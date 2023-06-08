c6f3d653bcd750b085db389bf9e9a7fe4f084850
SeanDowds <101082972+SeanDowds@users.noreply.github.com>
2023-06-08 08:39:11 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ anvil.server.connect("server_7H4WUF46LHBZE5AZNNVOPM4S-VFUBAVKSCYH72RXM")
 
 @anvil.server.callable
 def say_hi(name):
-  response = ("Hello from you Macbook M1 uplink, %s!" % name)
-  print('You are in your own Terminal - ',response)
+  response = ("Hello from Heroku uplink, %s!" % name)
+  print('You are in the Heroku Log - ',response)
   return response
 
 anvil.server.wait_forever()

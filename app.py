535c1d39332b38eeaf5beda674f1c3daa8878225
SeanDowds <101082972+SeanDowds@users.noreply.github.com>
2023-06-08 08:37:20 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import anvil.server
 anvil.server.connect("server_7H4WUF46LHBZE5AZNNVOPM4S-VFUBAVKSCYH72RXM")
 
 @anvil.server.callable
-def say_hello(name):
+def say_hi(name):
   response = ("Hello from you Macbook M1 uplink, %s!" % name)
   print('You are in your own Terminal - ',response)
   return response

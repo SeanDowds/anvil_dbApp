455d9db50fa082b2012c36a83e6b276a913df280
SeanDowds <101082972+SeanDowds@users.noreply.github.com>
2023-06-08 08:35:44 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+import anvil.server
+
+anvil.server.connect("server_7H4WUF46LHBZE5AZNNVOPM4S-VFUBAVKSCYH72RXM")
+
+@anvil.server.callable
+def say_hello(name):
+  response = ("Hello from you Macbook M1 uplink, %s!" % name)
+  print('You are in your own Terminal - ',response)
+  return response
+
+anvil.server.wait_forever()
